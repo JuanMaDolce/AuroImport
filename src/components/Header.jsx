d700dd9bade627f97fc2c12703c83fd9b54453ec
Juan Manuel Dolce <jmdolce@hotmail.com>
2023-09-16 21:08:39 -0300

    Configure React app for deployment to GitHub Pages

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,8 @@ import '../styles/Header.css'
 function Header() {
     return (
       <div className='header'>
-        <p>AUROIMPORT</p>
+          <img src={require("../assets/Images/header-background.jpg")} alt="header-background" />
+          <h1>AUROIMPORT</h1>
       </div>
     );
   }
